d350b26b2ebacefe3761ae91ac9da26dee582a77
m0wn1ka <rambabu9866@gmail.com>
2024-01-18 01:32:35 -0500

    news scraping

---
+++ news_scraping/README.md
@@ -0,0 +1,7 @@
+# requirements
+- use urllib and bs4 to get the news
+- parse and write news to a file with the date
+# advanced
+- make it as a cron job(collecting news)
+- publish them in some manner,say in our site,like gitub daily auto push
+- use selenium
---
+++ news_scraping/scrape.py
@@ -0,0 +1,26 @@
+a="""            ___                __ _         
+            / _ \              /_ | |        
+  _ __ ___ | | | |_      ___ __ | | | ____ _ 
+ | '_ ` _ \| | | \ \ /\ / / '_ \| | |/ / _` |
+ | | | | | | |_| |\ V  V /| | | | |   < (_| |
+ |_| |_| |_|\___/  \_/\_/ |_| |_|_|_|\_\__,_|"""
+print(a)
+
+
+#ref site  https://realpython.com/python-web-scraping-practical-introduction/
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+import csv
+#url="https://www.thehindu.com/news/international"
+#url="https://timesofindia.indiatimes.com/world"
+url="https://www.eenadu.net/world"
+page=urlopen(url)
+html=page.read().decode('utf-8')
+soup=BeautifulSoup(html,'html.parser')
+news1=soup.find_all("h3",class_="fnt20 article-title-rgt")
+with open("today.txt","w") as f:
+
+	for i in news1:
+		f.write(i.text)
+		print(i.text)
+		f.write("\n")
